d37fe5f3e4645c44acb0378363aa854361ba0bd4
Jooyul Yoon <juyeolyoon@gmail.com>
2021-12-28 12:49:31 +0900

    Add category buttons

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,32 @@
+import React from "react";
 import { IToDo } from "../atoms";
-function ToDo({ id, text }: IToDo) {
+
+function ToDo({ text, category }: IToDo) {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log(name);
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "To_Do" && (
+        <button name="To_Do" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "Doing" && (
+        <button name="Doing" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "Done" && (
+        <button name="Done" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
