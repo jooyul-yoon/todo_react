cc511ff7f8811d9279a86436acd404934f2fb125
Jooyul Yoon <juyeolyoon@gmail.com>
2021-12-30 18:32:25 +0900

    refactore

---
+++ src/components/DraggableCard.tsx
@@ -0,0 +1,36 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import { IToDo } from "../atoms";
+
+const Card = styled.li`
+  display: flex;
+  background-color: ${(props) => props.theme.cardColor};
+  width: 100%;
+  margin: 2px 0;
+  padding: 5px;
+  border-radius: 5px;
+`;
+
+interface ICardProps {
+  toDo: IToDo;
+  index: number;
+}
+
+function DraggableCard({ toDo, index }: ICardProps) {
+  console.log(toDo, "has been rendered");
+  return (
+    <Draggable key={toDo.id} draggableId={toDo.id.toString()} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
+          {toDo.text}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default DraggableCard;
