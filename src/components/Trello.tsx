50367d9697ffb08c1daf95aeea015ffe306dd4f5
Jooyul Yoon <juyeolyoon@gmail.com>
2021-12-30 11:14:29 +0900

    Beautiful DND set up

--- src/components/Trello.tsx
+++ src/components/Trello.tsx
@@ -1,36 +1,29 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { hourSelector, minuteState } from "../atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import Navigator from "./Navigator";
 
 function Trello() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  // const { register, watch, handleSubmit } = useForm();
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-
+  const onDragEnd = () => {};
   return (
     <>
       <Navigator />
-      <div>
-        <input
-          value={minutes}
-          onChange={onMinuteChange}
-          type="number"
-          placeholder="Minutes"
-        ></input>
-        <input
-          value={hours}
-          onChange={onHoursChange}
-          type="number"
-          placeholder="Hours"
-        ></input>
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <ul ref={magic.innerRef} {...magic.droppableProps}>
+                <Draggable draggableId="first" index={0}>
+                  {(magic) => (
+                    <li ref={magic.innerRef} {...magic.draggableProps}>
+                      <span {...magic.dragHandleProps}>🔥</span>
+                      First
+                    </li>
+                  )}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
     </>
   );
 }
