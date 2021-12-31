1b1b8bbd0c4c6d4c98acfc8eca00bbc6b6c4051f
Jooyul Yoon <juyeolyoon@gmail.com>
2021-12-31 21:08:14 +0900

    logo

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import TableIcon from "../icons/0cells.png";
 import TaskIcon from "../icons/0list-text.png";
+import logo from "../icons/logo.svg";
 
 const Header = styled.header`
   background-color: #fff;
@@ -48,10 +49,7 @@ function Navigator() {
     <Header>
       <NavContainer>
         <HomeIconContainer>
-          <img
-            src="https://camo.githubusercontent.com/48d099290b4cb2d7937bcd96e8497cf1845b54a810a6432c70cf944b60b40c77/68747470733a2f2f7261776769742e636f6d2f676f72616e67616a69632f72656163742d69636f6e732f6d61737465722f72656163742d69636f6e732e737667"
-            alt="react"
-          />
+          <img src={logo} alt="react" />
           <span>React Master</span>
         </HomeIconContainer>
         <TabContainer>
